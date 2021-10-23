e464d14a9e6325f4a94dc3db3f57ef24a345a361
Tomola-n <tomoya1106@gmail.com>
2021-10-23 21:15:49 +0900

    Final Project

--- finalproject.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-app.use("/", express.static(__dirname + '/'));
-app.get('/', (req, res) => { res.redirect("/index.html"); });
-
-http.listen(3000, () => {
-    console.log('listen 3000');
-});
